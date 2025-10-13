05428648d2e46b2508418d9b18a7f4c8654dd03c
aos-svg <artur.osinski@vonage.com>
2025-10-13 22:32:45 +0200

    VIDECO-11298 add repo

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+    name: "VonageClientSDKVideo",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "VonageClientSDKVideo",
+            targets: ["OpenTok", "VonageClientSDKVideo"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "OpenTok",
+                      url: "https://s3.us-east-1.amazonaws.com/artifact.tokbox.com/rel/otkit-ios-sdk-xcframework/vonage-ios-sdk-2.31.1.zip",
+                      checksum: "635b187917c0e97fb1fe060537217d1ea949b0e2c0be0f864849ffa028587fa0"),
+        .target(name: "VonageClientSDKVideo",
+                path: "Sources",
+                resources: [
+                    .process("VonageClientSDKVideo/Resources/selfie_segmentation.tflite"),
+                    .copy("VonageClientSDKVideo/Resources/PrivacyInfo.xcprivacy")
+                ],
+                linkerSettings: [
+                    .linkedFramework("Network"),
+                    .linkedFramework("VideoToolbox"),
+                    .linkedFramework("Accelerate"),
+                    .linkedLibrary("c++")
+                ]),
+        .testTarget(name: "VonageClientSDKVideoTests",
+                    dependencies: ["VonageClientSDKVideo"])
+    ]
+)
